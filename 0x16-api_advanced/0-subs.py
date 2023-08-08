6624d394c9fca6981338079b55073478123b5b4c
edddddr <endt6342@gmail.com>
2023-08-08 05:09:54 +0000

    the main tasksReddit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Modules T"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """a function that queries the Reddit API
+and returns the number of subscribers """
+
+    retrived = requests.get("https:/www.reddit.com/r/{}/about.json".
+                            format(subreddit),
+                            headers={"User-Agent": "KIDIST-123"})
+    if retrived.status_code != 200:
+        return 0
+    else:
+        return retrived.json().get("data").get("subscribers")
---
+++ 0x16-api_advanced/README.md
@@ -0,0 +1,1 @@
+More api_advanced
