a6cdb24ba9c62054d2277b58cfe445b64301d75b
edddddr <endt6342@gmail.com>
2023-08-08 18:28:28 +0000

    Fix the error

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
-"""Modules T"""
+"number of subscribers"
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """a function that queries the Reddit API
-and returns the number of subscribers """
-
-    retrived = requests.get("https:/www.reddit.com/r/{}/about.json".
-                            format(subreddit),
-                            headers={"User-Agent": "KIDIST-123"})
-    if retrived.status_code != 200:
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    data = response.json()
+    if response.status_code != 200:
         return 0
     else:
-        return retrived.json().get("data").get("subscribers")
+        return data['data']['subscribers']
