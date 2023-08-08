2ac6cf22253cfb2983c6b843b3596334b9fdb065
edddddr <endt6342@gmail.com>
2023-08-08 05:29:31 +0000

    fixed

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+#fix the wordepress content with apache
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php; sudo service apache2 restart',
+  path    => ['/bin', '/usr/bin', '/usr/sbin']
+}
