d1b45fea4fe0e1749c6497e9b57e17bd41f4c217
edddddr <endt6342@gmail.com>
2023-08-17 19:34:14 -0700

    two

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,13 @@
+# Ensure that Holberton can successfully log in and acces.
+
+# Hard file limit increase for Holberton users.
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# Increase the user's soft file limit on Holberton.
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
